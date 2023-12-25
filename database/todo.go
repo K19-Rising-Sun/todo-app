706b24bb4deae1c11f3ddefbb1f90b7169bdb645
AkagiYuu <khanghuynh0245@gmail.com>
2023-12-25 08:50:35 +0700

    feat(database): use sqlite fts5

--- database/todo.go
+++ database/todo.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"fmt"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -17,13 +19,46 @@ func (todo *Todo) Update(db *sqlx.DB) error {
 	return err
 }
 
-func GetTodos(db *sqlx.DB, username string) ([]Todo, error) {
+func QueryTodo(db *sqlx.DB, username string, category string, title string) ([]Todo, error) {
 	todos := []Todo{}
+	if category != "" && title != "" {
+		err := db.Select(&todos, `SELECT * FROM todo WHERE username = ? AND id IN(
+            SELECT rowid FROM todo_search WHERE todo_search MATCH ('category:' || ? || ' AND ' || ' title:' || ?)
+        )`, username, category, title)
+		return todos, err
+	}
+	if category != "" {
+		err := db.Select(&todos, `SELECT * FROM todo WHERE username = ? AND id IN(
+            SELECT rowid FROM todo_search WHERE todo_search MATCH ('category:' || ?)
+        )`, username, category)
+		fmt.Println(todos)
+		return todos, err
+	}
+	if title != "" {
+		err := db.Select(&todos, `SELECT * FROM todo WHERE username = ? AND id IN (
+            SELECT rowid FROM todo_search WHERE todo_search MATCH ('title:' || ?)
+        )`, username, title)
+		fmt.Println(todos)
+		return todos, err
+	}
+
 	err := db.Select(&todos, "SELECT * FROM todo WHERE username = ?", username)
+	fmt.Println(todos)
 	return todos, err
 }
 
-func DeleteTodo(db *sqlx.DB, id int) error {
-	_, err := db.Exec("DELETE FROM todo WHERE id = ?", id)
-	return err
+func DeleteTodo(db *sqlx.DB, id int, username string) error {
+	result, err := db.Exec("DELETE FROM todo WHERE username = ? AND id = ?", username, id)
+    if err != nil {
+        return err
+    }
+
+    row_affected_count, err := result.RowsAffected()
+    if err != nil {
+        return err
+    }
+    if row_affected_count == 0 {
+        return fmt.Errorf("User %s has no todo with id %d", username, id)
+    }
+    return nil
 }
